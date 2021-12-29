05807ce66c0e6d8534e9018623a2500107f2b0d5
Nazar Shpak <nazar.shpak2004@gmail.com>
2021-12-29 23:33:22 +0200

    fixed code

--- Lab_3.cpp
+++ Lab_3.cpp
@@ -1,26 +1,32 @@
 #include <stdio.h>
-//створюю функцію
-void test(char (*words)[10], int wordsCount, char* word){
+
+
+void crossword(char(*words)[10], int wordsCount, char* word)
+
+{
     int counter = 0;
     for (int j = 0; j < wordsCount; j++)
     {
         for (int i = 0; i < wordsCount; i++)
         {
             word[i] = words[i][counter];
-if(words[i][counter]==  ' ')
- break;
+            if (words[i][counter] == ' ')
+                break;
+
         }
-        if (word[wordsCount-1])
+        if (word[wordsCount - 1])
             break;
         counter++;
     }
 }
 
-int main(){
-    char word[3];
-    char words[3][10] = {"cat", " salad", " kiwi"};
-    test(words,3,word);
+int main()
+{
+    const int A = 3, B = 10;
+    char word[A];
+    char words[A][B] = { "cat", " salad", " kiwi" };
+    crossword(words, A, word);
     printf("%s", word);
-//виводить результат
+
     return 0;
 }
